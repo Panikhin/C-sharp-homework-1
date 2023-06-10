11bbf7ed994999677929912b46153f92561739a2
Дмитрий Панихин <dpanikhin@gmail.com>
2023-06-11 01:00:16 +0300

    task_1_6 done

--- Task_1_6/Program.cs
+++ Task_1_6/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear;
-Console.WriteLine("Hello, World!");
+﻿// Задача: напишите программу, которая на вход принимает число и выдает, являетли ли число четным
+Console.Clear();
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine($"Number {number} is even");
+else 
+    Console.WriteLine($"Number {number} is odd");
