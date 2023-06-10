e9c46266a491255cae01f5471f56aa6dce16f159
Дмитрий Панихин <dpanikhin@gmail.com>
2023-06-11 00:27:03 +0300

    tas_1_4 done

--- Task_1_4/Program.cs
+++ Task_1_4/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача: напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (max < numberB) max = numberB;
+if (max < numberC) max = numberC;
+Console.WriteLine($"Максимальное числе из трех равно {max}");
+
